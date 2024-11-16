b65992aa828962d9fdc713eb4230083daaf518e2
m1ngsama <1992584620@nbt.edu.cn>
2024-11-17 03:52:03 +0800

    add telescope and some related keymap

--- init.lua
+++ init.lua
@@ -27,8 +27,10 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
 vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
-vim.keymap.set("n", "<Leader>v", "<C-w>v",opt)
-vim.keymap.set("n", "<Leader>s", "<C-w>s",opt)
+vim.keymap.set("n", "<leader>v", "<C-w>v", opt)
+vim.keymap.set("n", "<leader>s", "<C-w>s", opt)
+vim.keymap.set("n", "<leader>[", "<C-o>", opt)
+vim.keymap.set("n", "<leader>]", "<C-i>", opt)
 
 -- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
@@ -51,6 +53,18 @@ require("lazy").setup({
 	{
 		"RRethy/nvim-base16",
 		lazy = true,
+	},
+	{
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>p", ":Telescope find_files<CR>", desc = "find files" },
+			{ "<leader>P", ":Telescope live_grep<CR>", desc = "grep files" },
+			{ "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
+			{ "<leader>q", ":Telescope oldfiles<CR>", desc = "old files" },
+		},
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.3',
+		dependencies = { 'nvim-lua/plenary.nvim' },
 	},
 })
 
